f58f4dae9a970b7a411572371341d7b5dd5ce23b
Mohammed Sajad Vellathi <mohdsajadvellathi@gmail.com>
2026-02-01 04:54:24 +0530

    Fix Home component casing for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./Home";
+import Home from "./src/Home";
 import DemoPage from "./components/DemoPage";
 
 function App() {
