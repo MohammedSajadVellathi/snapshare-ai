c713f3945f7baab1472076671f574ea0c3a20ad8
Mohammed Sajad Vellathi <mohdsajadvellathi@gmail.com>
2026-02-03 21:57:02 +0530

    Clean frontend deploy (recruiter ready)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,21 @@
-<button className="demo-btn">
-  Get a demo <span className="arrow">→</span>
-</button>
-
-
 import heroImage from "../assets/imag1.jpg";
 
 function Hero() {
   return (
     <section className="hero">
-      <span className="hero-tag animate-tag">
+      <h1 className="hero-tag animate-tag">
         SnapShare AI
-      </span>
+      </h1>
+
 
 
       <p className="hero-subtext">
         Instant AI Photo Delivery for Events and Photographers using {" "}
         <span className="highlight-text animate-highlight">AI Facial Recognition.</span>
       </p>
+      <button className="demo-btn">
+        Get a demo <span className="arrow">→</span>
+      </button>
 
       <div className="hero-image">
         <div className="image-wrapper">
@@ -26,5 +25,6 @@ function Hero() {
     </section>
   );
 }
+
 
 export default Hero;
